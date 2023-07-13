28530ffeaec7822d94a1e0aea8f1dede356af0a0
Daniel Vieira Alcantara <danielvieiraalcantara10@gmail.com>
2023-07-12 22:36:13 -0300

    Project using math.random in JS.

---
+++ math.js
@@ -0,0 +1,18 @@
+/*
+
+
+Math -> Matemática
+- pow(2,2) / potência / 2² = 4
+- sqrt(25,2) / raiz / √25
+- PI
+- ceil / teteo -> arredondar pra cima
+- floor / chão -> arredondar pra baixo
+
+- random() / número aleatório entre 0 e 1
+*/
+
+const result = Math.pow(2,6.7)
+console.log(result)
+
+
+
